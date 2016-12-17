ba665e5603cc36da067026377a67688bc0222aa2
Olga <demochko_ol@mail.ru>
2016-12-17 20:38:58 +0200

    script for main navigation

--- files/js/js.js
+++ files/js/js.js
@@ -3,12 +3,31 @@ $(function(){
 	
 	function setsHeaderHeight() {
 		var wheight = $(window).height();
-		$(".header").css('height', wheight);
+		if(wheight>320){
+			$(".header").css('height', wheight);
+		}else{
+			$(".header").css('height', 320);
+		}
+		
 	}
-
-
 	setsHeaderHeight();
+
+	$(window).resize(function(){
+		setsHeaderHeight();
+	});
+
+	$('.main-nav ul').on("click", 'a', function(event){
+        event.preventDefault();
+
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -39;
+            $('body,html').animate({scrollTop: top}, 1000);    
+        
+    });
+
 	$(window).scroll(function(){		
-		$(".main-nav").toggleClass('fixed-top', $(window).scrollTop()>$('.header').outerHeight());		
-	})
+		$(".main-nav").toggleClass('fixed-top', $(window).scrollTop()>=$('.header').outerHeight());		
+	});
+
+
 });
