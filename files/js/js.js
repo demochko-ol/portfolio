c2c93a515af6418a1e15b32502d4ac879a4c9956
Olga <demochko_ol@mail.ru>
2016-12-19 23:26:31 +0200

    fully responsive about & skills sections. added Scrollspy to main navigation

--- files/js/js.js
+++ files/js/js.js
@@ -11,94 +11,25 @@ $(function(){
 		
 	}
 	setsHeaderHeight();
-	/*$(".nav").toggleClass('hidden', $(window).width()<768);*/
+	
 	$(window).resize(function(){
 		setsHeaderHeight();
 
-		// $(".nav").css("display", $(window).width()>=768 ? "inline-block" : "none");
-		/*$(".nav").toggleClass('hidden', $(window).width()<768);*/
 		if($(window).width()>767){
-			$(".nav").css("height", "auto");
-		}else{
-			var nav_height = $(".nav ul").height();
-
-			$('.nav').css("height", $(".nav").hasClass('open') ? nav_height : 0);
+			$(".nav").removeAttr("style");
 		}
-				
 	});
 	
 	$(".toggle-nav").on("click", function(event){		
 		event.preventDefault();
 
-		var nav_height = $(".nav ul").height();
-		
-		$(".nav").toggleClass('open');
+		$(".nav").slideToggle(500, function(){
+			$(this).toggleClass("open", $(this).is(":visible"));
+		});
 
-		if($(".nav").hasClass('open')){
-			$(".nav").animate({
-				height: nav_height/*,
-				specialEasing: {
-					height: 'linear'
-				}*/
-			}, 500);
-		}else{
-			$(".nav").animate({
-				height: 0/*,
-				specialEasing: {
-					height: 'linear'
-				}*/
-			}, 500);
-		}
-
-
-		/*$(".hidden").slideToggle(400);*/
-
-
-		/*if($(window).width()=<767){
-			$(".nav ul").toggleClass('open');
-		}*/
-
-		/*if($(".nav ul").css("display")=="none"){
-			
-			$(".nav ul").animate({
-				height: 'show',
-				specialEasing: {
-					height: 'linear'
-				}
-			}, 500);
-		}else{
-			$(".nav ul").animate({
-				height: 'hide',
-				specialEasing: {
-					height: 'linear'
-				}
-			}, 500);
-		}*/
-
-/*
-		$(".main-nav ul").animate({
-			height: "toggle"
-		},{duration:500,
-			specialEasing: {
-				height: 'linear'
-			}
-		});*/
-
-	/*	if($(".main-nav ul").css("display")=="none"){
-			
-		}else{
-			$(".main-nav ul").animate({
-				height: 'hide',
-				specialEasing: {
-					height: 'linear'
-				}
-			}, 500);
-		}
-*/
-		
 	});
 
-	$('.main-nav ul').on("click", 'a', function(event){
+	/*$('.main-nav ul').on("click", 'a', function(event){
         event.preventDefault();
 
         var link = $(this),
@@ -107,14 +38,64 @@ $(function(){
 
             link.addClass("active").parent().siblings().find('a').removeClass("active");
             $('body,html').animate({scrollTop: top}, 1000);         
-    });
+    });*/
 
-	$(window).scroll(function(){		
-		$(".main-nav").toggleClass('fixed-top', $(window).scrollTop()>=$('.header').outerHeight());		
 
-		
 
-	});
+	// Cache selectors
+var lastId,
+  topMenu = $(".main-nav ul"),
+  topMenuHeight = topMenu.outerHeight() + 15,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) {
+      return item;
+    }
+  });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+  $('html, body').stop().animate({
+    scrollTop: offsetTop
+  }, 1300);
+  e.preventDefault();
+});
+
+	
+// Bind to scroll
+$(window).scroll(function() {
+
+	$(".main-nav").toggleClass('fixed-top', $(window).scrollTop()>=$('.header').outerHeight()-15);	
+
+
+
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function() {
+    if ($(this).offset().top < fromTop)
+      return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent().removeClass("active")
+      .end().filter("[href='#" + id + "']").parent().addClass("active");
+  }
+});
+
 
 
 });
